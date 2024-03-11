f125a4cf8e8173dc381035e6f5b4b896759cf721
Mayank <mayanksingh12@gmail.com>
2024-03-11 09:51:36 +0530

    add: animation handler for default page gradient animation

--- src/components/Authentication/Default.jsx
+++ src/components/Authentication/Default.jsx
@@ -25,24 +25,44 @@ const Default = () => {
 
   return (
     <>
-      <div className="flex" >
-        <BackgroundGradientAnimation
-          className=" absolute top-0 left-0 hidden w-2/4 h-screen p-14 lg:flex flex-col justify-between transition-all"
-        >
-          <div >
-            <h1 className="text-white font-extrabold text-[4rem] font-mont">
-              ScriptQuiz
-            </h1>
-            <h3 className="text-white font-madurai">
-              Ace your next frontend interview with confidence.
-            </h3>
-          </div>
-          <span className="text-white font-madurai">
-            Created with <span className="animate-pulse">❤️</span> by Mayank.
-          </span>
-        </BackgroundGradientAnimation>
+      <div className="flex">
+        {showAnimation ? (
+          <>
+            <BackgroundGradientAnimation className="absolute top-0 left-0 hidden w-2/4 h-screen p-14 lg:flex flex-col justify-between transition-all">
+              <div>
+                <h1 className="text-white font-extrabold text-[4rem] font-mont">
+                  ScriptQuiz
+                </h1>
+                <h3 className="text-white font-madurai">
+                  Ace your next frontend interview with confidence.
+                </h3>
+              </div>
+              <span className="text-white font-madurai">
+                Created with <span className="animate-pulse">❤️</span> by
+                Mayank.
+              </span>
+            </BackgroundGradientAnimation>
+          </>
+        ) : (
+          <>
+            <div className="hidden w-2/4 h-screen p-14 lg:flex flex-col justify-between bg-gradient-to-tr from-[#160040] from-10% via-[#4C0070] via-30% via-[#79018C] via-60% to-[#9A0680] to-90% ">
+              <div className="z-10">
+                <h1 className="text-white font-extrabold text-[4rem] font-mont">
+                  ScriptQuiz
+                </h1>
+                <h3 className="text-white font-madurai">
+                  Ace your next frontend interview with confidence.
+                </h3>
+              </div>
+              <span className="text-white font-madurai">
+                Created with ❤️ by Mayank.
+              </span>
+            </div>
+          </>
+        )}
+
         <div className="lg:w-2/4  w-full h-screen p-14 relative flex flex-col justify-center items-center z-10">
-        {showAnimation ? <BackgroundBeams /> : <></>}
+          {showAnimation ? <BackgroundBeams /> : <></>}
           <nav className="w-full px-5 items-center fixed top-0 left-0 right-0 m-auto flex justify-between lg:hidden py-3 backdrop-blur-sm shadow-md z-20">
             <h1 className="text-white font-extrabold text-[1.5rem] font-mont">
               ScriptQuiz
@@ -54,7 +74,7 @@ const Default = () => {
               >
                 <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-2 px-5 ring-1 ring-white/10 ">
                   <span>{logType ? <>SignUp</> : <>Login</>}</span>
-                <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+                  <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
                 </div>
               </button>
               <button
@@ -73,7 +93,7 @@ const Default = () => {
                 )}
               </button>
             </div>
-                <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
           </nav>
 
           <button
@@ -103,7 +123,6 @@ const Default = () => {
               </>
             )}
           </button>
-        
         </div>
       </div>
     </>
